5bcacd3deaa3e459dee793e3f40ceaf34d591e30
Chadd Poggenpoel <chaddpoggenpoel@gmail.com>
2020-09-20 23:51:10 +0200

    0.0.1

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Header from './header'
-
 import '../../static/fonts/fonts.css'
 
 function Layout({ children }) {
   return (
     <div>
-      <Header />
-
-      <main className='px-12 py-16 bg-gray-400'>
-        <div className='w-full h-auto border-t-8 border-l-8 border-r-8 border-gray-100 border-solid shadow-2xl background'>
+      <main className='bg-gray-400 lg:px-24 lg:py-32'>
+        <div className='w-full h-auto lg:border-t-8 lg:border-l-8 lg:border-r-8 lg:border-gray-100 lg:border-solid lg:shadow-2xl background'>
           {children}
         </div>
 
-        <footer className='py-4 text-xs text-center text-gray-100 border-b-8 border-l-8 border-r-8 border-gray-100 border-solid background-footer'>
+        <footer className='py-4 text-xs text-center text-gray-100 lg:border-b-8 lg:border-l-8 lg:border-r-8 lg:border-gray-100 lg:border-solid background-footer'>
           <p>Copyright ©Core Water, 2020. All rights reserved.</p>
           <p>
             JAMstack Website by {''}
